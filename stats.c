c309e8ddd3181e40f41e10bc1636b596732cec07
Ankush Burman <ankushburman.ab@gmail.com>
2018-10-07 20:54:40 -0700

    Adding function definitions and comments to stats.h

--- stats.c
+++ stats.c
@@ -40,4 +40,5 @@ void main() {
 
 }
 
+
 /* Add other Implementation File Code Here */
--- stats.h
+++ stats.h
@@ -36,5 +36,103 @@
  * @return <Add Return Informaiton here>
  */
 
+/**
+ * @brief Function prints statistics of array
+ *
+ * Function iterates over array and prints out statistics such as
+ * minimum, maximum, mean, and median.
+ *
+ * @param array passing array to be computed on by reference
+ * @param len the length of array
+ *
+ * @return returns 0
+ */
+
+ void print_statistics(unsigned char *array, int len);
+
+
+/**
+ * @brief Function prints contents of array
+ *
+ * Function iterates over array and prints out all its contents
+ *
+ * @parm array passing array by reference
+ * @parm len passing the length of the array as an int
+ *
+ * @return returns 0
+ */
+
+ void print_array(unsigned char *array, int len);
+
+
+/**
+ * @brief Function finds the median
+ *
+ * Function finds median of all the values held in the array
+ * 
+ * @parm array passing array by reference
+ * @parm len passing the length of the array as an int
+ *
+ * @return returns median
+ */
+
+ int find_median(unsigned char *array, int len);
+
+
+/**
+ * @brief Function finds the mean
+ *
+ * Function finds mean of all the values held in the array
+ * 
+ * @parm array passing array by reference
+ * @parm len passing the length of the array as an int
+ *
+ * @return returns mean
+ */
+
+ int find_mean(unsigned char *array, int len);
+
+
+/**
+ * @brief Function finds the maximum
+ *
+ * Function finds maximum our of all the values held in the array
+ * 
+ * @parm array passing array by reference
+ * @parm len passing the length of the array as an int
+ *
+ * @return returns maximum
+ */
+
+ int find_maximum(unsigned char *array, int len);
+
+ 
+ /**
+ * @brief Function finds the minimum
+ *
+ * Function finds minimum out of all the values held in the array
+ * 
+ * @parm array passing array by reference
+ * @parm len passing the length of the array as an int
+ *
+ * @return returns minimum
+ */
+
+ int find_minimum(unsigned char *array, int len);
+
+
+/**
+ * @brief Function sorts the array 
+ *
+ * Function sorts the array from largest to smallest
+ * 
+ * @parm array passing array by reference
+ * @parm len passing the length of the array as an int
+ *
+ * @return returns 0
+ */
+
+ void sort_array(unsigned char *array, int len);
+
 
 #endif /* __STATS_H__ */
